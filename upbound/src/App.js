b3333e9291af8859ecaa4de87e2571e913189dd9
hilal aissani <hilalaissani@gmail.com>
2019-01-15 18:27:41 -0500

    stats"

--- upbound/src/App.js
+++ upbound/src/App.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "./css/App.css";
+import NavBar from "./navBar/navbar";
+import Cards from "./cards/cards";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>welcome to react app</h1>
+        <NavBar />
+        <Cards />
       </div>
     );
   }
---
+++ upbound/src/cards/cards.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Cards extends Component {
+  render() {
+    return (
+      <div className="cards">
+        <h3>Cards</h3>
+      </div>
+    );
+  }
+}
+export default Cards;
